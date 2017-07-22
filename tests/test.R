660d2cd1121cffabde0b85bef51bcad7f1d356d0
Anthony Damico <ajdamico@gmail.com>
2017-07-22 07:35:54 -0400

    machine_specific_replacements

--- tests/test.R
+++ tests/test.R
@@ -6,8 +6,6 @@ machine_specific_replacements <-
 		
 		# change other things in the script to be run
 		c( "hello" , "howdy" )
-		
 	)
 
 source( lodown::syntaxtractor( "yrbss" , replacements = machine_specific_replacements , setup_test = "test" ) , echo = TRUE )
-
