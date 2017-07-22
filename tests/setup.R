0311b5da0a5d1e881a3d187e6c19a3a7b5b5cab4
Anthony Damico <ajdamico@gmail.com>
2017-07-22 16:51:17 -0400

    setup automation

--- tests/setup.R
+++ tests/setup.R
@@ -1,12 +1,4 @@
-machine_specific_replacements <- 
-	list( 
-		
-		# replace the folder path on macnix
-		c( 'path.expand( \"~\" ) , \"YRBSS\"' , paste0( '"' , getwd() , '"' ) ) ,
-		
-		# change other things in the script to be run
-		c( "hello" , "howdy" )
-	)
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
-source( lodown::syntaxtractor( "yrbss" , replacements = machine_specific_replacements , setup_test = "setup" ) , echo = TRUE )
+library(lodown)
+lodown( "yrbss" , output_dir = file.path( getwd() ) )
