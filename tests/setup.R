03a6310e625f4ffb5df787996dd59c6aa66a1695
Anthony Damico <ajdamico@gmail.com>
2017-07-22 08:25:00 -0400

    does appveyor allow paging

--- tests/setup.R
+++ tests/setup.R
@@ -7,5 +7,6 @@ machine_specific_replacements <-
 		# change other things in the script to be run
 		c( "hello" , "howdy" )
 	)
+if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
 source( lodown::syntaxtractor( "yrbss" , replacements = machine_specific_replacements , setup_test = "setup" ) , echo = TRUE )
