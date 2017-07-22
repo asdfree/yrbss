4a8432dc868890106d51b05ab1f2172dc075fd35
Anthony Damico <ajdamico@gmail.com>
2017-07-22 03:40:15 -0400

    basics

---
+++ tests/yrbss-test.R
@@ -0,0 +1,13 @@
+machine_specific_replacements <- 
+	list( 
+		
+		# replace the folder path on macnix
+		c( 'path.expand( \"~\" ) , \"YRBSS\"' , paste0( '"' , getwd() , '"' ) ) ,
+		
+		# change other things in the script to be run
+		c( "hello" , "howdy" )
+		
+	)
+
+source( lodown::syntaxtractor( "yrbss" , replacements = machine_specific_replacements , setup_test = "test" ) , echo = TRUE )
+
